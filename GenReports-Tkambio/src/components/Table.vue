5880515e5f1577d28be8361e982dad78e5b3e1dc
Daniel Ortiz <daniel_ortizpary@hotmail.com>
2022-12-24 18:08:51 -0500

    table add

---
+++ GenReports-Tkambio/src/components/Table.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="table-background">
+    <table>
+      <thead>
+        <tr>
+          <th>Titulo</th>
+          <th></th>
+          <th>Fecha de creación</th>
+          <th></th>
+          <th>Acción</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Reporte de usuario 1</td>
+          <td class="separator"><div class="separator-line"></div></td>
+          <td>04/02/2022</td>
+          <td class="separator"><div class="separator-line"></div></td>
+          <td>
+            Descargar
+            <img src="../assets/svgs/download-icon.svg" alt="download icon" />
+          </td>
+        </tr>
+        <tr>
+          <td>Reporte de usuario 2</td>
+          <td class="separator"><div class="separator-line"></div></td>
+          <td>04/02/2022</td>
+          <td class="separator"><div class="separator-line"></div></td>
+          <td>
+            Descargar
+            <img src="../assets/svgs/download-icon.svg" alt="download icon" />
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
