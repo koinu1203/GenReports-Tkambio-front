30490c64b39f1b00f83a1b7e239be7fd92bee305
Daniel Ortiz <daniel_ortizpary@hotmail.com>
2022-12-24 18:11:46 -0500

    main page create

--- GenReports-Tkambio/src/App.vue
+++ GenReports-Tkambio/src/App.vue
@@ -1,12 +1,27 @@
 <script setup>
-import Header from './components/Header.vue'
-const name = "Daniel";
+import Header from './components/Header.vue';
+import ReportTable from './components/Table.vue';
+import ReportModal from './modals/ReportModal.vue';
 </script>
 
 <template>
+  <ReportModal/>
   <Header/>
+  <h1>Generador de reportes</h1>
+  <ReportTable/>
+  <button> Crear reporte</button>
+
 </template>
 
-<style>
+<style lang="scss">
   @import './styles/_main.scss';
+  h1{
+    margin-top: 55px;
+    margin-bottom: 96px;
+  }
+  button{
+    margin-top: 30px;
+  }
+  // @import './styles/_fonts.scss';
+
 </style>
