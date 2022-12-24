2f50b4598db1c0df0459bfbc15cb9fec82aa38f8
Daniel Ortiz <daniel_ortizpary@hotmail.com>
2022-12-24 18:11:03 -0500

    scss fix

--- GenReports-Tkambio/vite.config.js
+++ GenReports-Tkambio/vite.config.js
@@ -5,22 +5,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  css:{
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
-          @import "./src/styles/_animations.scss";
-          @import "./src/styles/_variables.scss";
-          @import "./src/styles/_mixins.scss";
-          @import "./src/styles/_helpers.scss";
-        `
-      }
-    }
-  },
+  
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  publicDir:'./public'
 })
