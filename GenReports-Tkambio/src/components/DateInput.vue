64725df1884ebc3e4822e2217deae52d61ce87ba
Daniel Ortiz <daniel_ortizpary@hotmail.com>
2022-12-24 19:46:30 -0500

    date input create

--- GenReports-Tkambio/src/components/DateInput.vue
+++ GenReports-Tkambio/src/components/DateInput.vue
@@ -4,13 +4,18 @@ export default {
   props: {
     label: String,
   },
+
 };
+const onDatePick = ()=>{
+  console.log('hola');
+}
 </script>
 
 <template>
-  <div class="input-text">
-    <div class="input-text-content">
-      <input type="text" />
+  <div class="input-date-text">
+    <div class="input-date-text-content">
+      <input type="text" readonly/>
+      <a :on-click="onDatePick"><img class= "icon" src="../assets/svgs/calendar-icon.svg" alt="calendar icon" /></a>
     </div>
     <span>{{ label }}</span>
   </div>
