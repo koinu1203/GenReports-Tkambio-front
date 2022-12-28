e044010fc6cde67ca8207b7afc739c9d2ff1b2f5
Daniel Ortiz <daniel_ortizpary@hotmail.com>
2022-12-28 18:09:11 -0500

    modifications in style and structure

--- GenReports-Tkambio/src/components/DateInput.vue
+++ GenReports-Tkambio/src/components/DateInput.vue
@@ -1,25 +1,103 @@
 <script>
+import Calendar from "./Calendar.vue";
 export default {
   name: "DateInput",
   props: {
     label: String,
+    inputDate:{
+      type:Date,
+      default:null
+    },
+    minDate:{
+      type:Date,
+      default:null
+    },
+    errorMinDate:{
+      type:String,
+      default:'La fecha no puede ser menor a la minima permitida'
+    },
+    maxDate:{
+      type:Date,
+      default:null
+    },
+    errorMaxDate:{
+      type:String,
+      default:'La fecha supera la maxima permitida'
+    }
   },
-
+  update(){
+    if(!this.inputDate){
+      return;
+    }
+    this.errorText='';
+    if(this.maxDate && Date.parse(this.maxDate)<Date.parse(this.inputDate)){
+      this.errorText=this.errorMaxDate;
+    }
+    if(this.minDate && Date.parse(this.minDate)>Date.parse(this.inputDate)){
+      this.errorText=this.errorMinDate;
+    }
+  },
+  components: {
+    Calendar,
+  },
+  methods: {
+    showDatePick() {
+      this.showCalendar = !this.showCalendar;
+    },
+    updateDate(event) {    
+      if(event){
+        this.dateText=this.formatDate(event);
+        this.showCalendar = false;
+        this.$emit('update-date',event);
+      }
+      this.errorText='';
+      if(this.maxDate && Date.parse(this.maxDate)<Date.parse(event)){
+        this.errorText=this.errorMaxDate;
+      }
+      if(this.minDate && Date.parse(this.minDate)>Date.parse(event)){
+        this.errorText=this.errorMinDate;
+      }
+    },
+    formatDate(date){
+      const month = date.getMonth()+1;
+      const day = date.getDate();
+      return `${day<10?'0':''}${day}/${month<10?'0':''}${month}/${date.getFullYear()}`;
+    }
+  },
+  data() {
+    return {
+      showCalendar: false,
+      dateText: '',
+      errorText: ''
+    };
+  }
 };
-const onDatePick = ()=>{
-  console.log('hola');
-}
 </script>
 
 <template>
-  <div class="input-date-text">
-    <div class="input-date-text-content">
-      <input type="text" readonly/>
-      <a :on-click="onDatePick"><img class= "icon" src="../assets/svgs/calendar-icon.svg" alt="calendar icon" /></a>
+  <div class="input-date-picker">
+    <div class="input-date-text">
+      <div class="input-date-text-content" :class="{'error-date':errorText}">
+        <input
+          type="text"
+          v-model="dateText"
+          readonly
+        />
+        <a @click="showDatePick()"
+          ><img
+            class="icon"
+            src="../assets/svgs/calendar-icon.svg"
+            alt="calendar icon"
+        /></a>
+      </div>
+      <span>{{ label }}</span>
     </div>
-    <span>{{ label }}</span>
-  </div>
+    <span v-if="errorText" class="input-date-error">{{ errorText }}</span>
 
+    <div v-if="showCalendar">
+      <Calendar :inputDate="inputDate" @update-date="updateDate($event)" />
+    </div>
+  </div>
 </template>
 
 <style lang="scss"></style>
