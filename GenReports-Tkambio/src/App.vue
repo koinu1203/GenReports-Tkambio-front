9578ef0fe9d4ab12cd14046fa7af1c5021d6a8a9
Daniel Ortiz <daniel_ortizpary@hotmail.com>
2022-12-28 18:07:25 -0500

    landing page created

--- GenReports-Tkambio/src/App.vue
+++ GenReports-Tkambio/src/App.vue
@@ -1,27 +1,14 @@
 <script setup>
-import Header from './components/Header.vue';
-import ReportTable from './components/Table.vue';
-import ReportModal from './modals/ReportModal.vue';
+
+import LandingPage from './LandingPage.vue';
+
 </script>
 
 <template>
-  <ReportModal/>
-  <Header/>
-  <h1>Generador de reportes</h1>
-  <ReportTable/>
-  <button> Crear reporte</button>
+  <LandingPage/>
 
 </template>
 
 <style lang="scss">
   @import './styles/_main.scss';
-  h1{
-    margin-top: 55px;
-    margin-bottom: 96px;
-  }
-  button{
-    margin-top: 30px;
-  }
-  // @import './styles/_fonts.scss';
-
 </style>
---
+++ GenReports-Tkambio/src/LandingPage.vue
@@ -0,0 +1,40 @@
+<script>
+import Header from './components/Header.vue';
+import ReportTable from './components/Table.vue';
+import ReportModal from './modals/ReportModal.vue';
+import Calendar from './components/Calendar.vue';
+export default{
+    name:'LandingPage',
+    components:{
+        ReportModal,
+        Header,
+        ReportTable,
+    },
+    data(){
+        return {
+            showModal:false
+        }
+    }
+}
+</script>
+
+<template>
+  <ReportModal v-if="showModal" @closeModal="()=> showModal = false"/>
+  <Header/>
+  <section>
+    <h1>Generador de reportes</h1>
+    <ReportTable/>
+    <button id="open-report-modal" @click="showModal=true"> Crear reporte</button>
+  </section>
+</template>
+
+<style lang="scss">
+  section h1{
+    margin-top: 55px;
+    margin-bottom: 96px;
+  }
+  button{
+    margin-top: 30px;
+  }
+
+</style>
