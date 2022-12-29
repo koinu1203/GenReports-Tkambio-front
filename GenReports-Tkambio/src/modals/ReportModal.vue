c0adf7c0a3a7c991f6949e9e0490c333ee7ecfe4
Daniel Ortiz <daniel_ortizpary@hotmail.com>
2022-12-28 20:27:46 -0500

    clickout event added

--- GenReports-Tkambio/src/modals/ReportModal.vue
+++ GenReports-Tkambio/src/modals/ReportModal.vue
@@ -22,72 +22,81 @@ export default {
       this.$emit("closeModal");
     },
     validCheck() {
-      if(this.description && this.initDate && this.finishDate && Date.parse(this.initDate)<=Date.parse(this.finishDate)){
-        this.isValidData=true;
-      }else{
-        this.isValidData=false;
+      if (
+        this.description &&
+        this.initDate &&
+        this.finishDate &&
+        Date.parse(this.initDate) <= Date.parse(this.finishDate)
+      ) {
+        this.isValidData = true;
+      } else {
+        this.isValidData = false;
       }
     },
-    updateInitDate(event){
+    updateInitDate(event) {
       this.initDate = event;
       this.validCheck();
     },
-    updateFinishDate(event){
+    updateFinishDate(event) {
       this.finishDate = event;
       this.validCheck();
-    }
+    },
   },
   data() {
     return {
       description: "",
       initDate: null,
       finishDate: null,
-      isValidData:false,
+      isValidData: false,
     };
-  }
+  },
 };
 </script>
 
 <template>
-  <div class="modal">
-    <div class="modal-body">
-      <h2 class="modal-title">Reporte por fecha de nacimiento</h2>
-      <h3 class="modal-subtitle">
-        Ingresa los siguientes datos para generar tu reporte
-      </h3>
-      <form class="modal-content" @submit.prevent="submitEvent()">
-        <Input
-          label="Descripcion del reporte"
-          @input="
-            (event) => {
-              description = event;
-              this.validCheck();
-            }
-          "
-          required
-        />
-        <span class="date-label">Fecha de nacimiento</span>
-        <div class="input-date">
-          <DateInput
-            label="Inicio"
-            :inputDate="initDate"
-            :maxDate="finishDate"
-            :errorMaxDate="'La fecha inicial es mayor a la final'"
-            @update-date="updateInitDate($event)"
+  <div class="modal" @click="closeModal()">
+      <div
+        class="modal-body"
+        
+        @click.stop
+      >
+        <h2 class="modal-title">Reporte por fecha de nacimiento</h2>
+        <h3 class="modal-subtitle">
+          Ingresa los siguientes datos para generar tu reporte
+        </h3>
+        <form class="modal-content" @submit.prevent="submitEvent()">
+          <Input
+            label="Descripcion del reporte"
+            @input="
+              (event) => {
+                description = event;
+                this.validCheck();
+              }
+            "
             required
           />
-          <DateInput
-            label="Fin"
-            :inputDate="finishDate"
-            :minDate="initDate"
-            :errorMinDate="'La fecha final es menor a la final'"
-            @update-date=" updateFinishDate($event)"
-            required
-          />
-        </div>
-        <button :disabled="!isValidData">Generar reporte</button>
-      </form>
-    </div>
+          <span class="date-label">Fecha de nacimiento</span>
+          <div class="input-date">
+            <DateInput
+              label="Inicio"
+              :inputDate="initDate"
+              :maxDate="finishDate"
+              :errorMaxDate="'La fecha inicial es mayor a la final'"
+              @update-date="updateInitDate($event)"
+              required
+            />
+            <DateInput
+              label="Fin"
+              :inputDate="finishDate"
+              :minDate="initDate"
+              :errorMinDate="'La fecha final es menor a la final'"
+              @update-date="updateFinishDate($event)"
+              required
+            />
+          </div>
+          <button :disabled="!isValidData">Generar reporte</button>
+        </form>
+      </div>
   </div>
 </template>
 
